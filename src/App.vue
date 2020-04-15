e4762b536df44f737e9ce0a7fe5be52c5daa97a0
Steven Kearns <skearns@vgimfs.com>
2020-04-14 20:56:26 -0400

    Fix header spacing issues.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <template>
   <v-app>
     <!-- TODO: Donate now button, in header social media links, 3 tabs one for pictures one for donations and one to contact us -->
-    <!-- 
-    <v-app-bar app absolute shrink-on-scroll prominent color="#0C2E5F" dark>-->
-    <v-app-bar app color="#0C2E5F" dark shrink-on-scroll prominent>
+    <!-- <v-app-bar app color="#0C2E5F" dark shrink-on-scroll prominent>-->
+    <v-app-bar app color="#0C2E5F" dark>
       <!-- <div class="d-flex align-center"></div> -->
-      <v-img
-        :src="require('./assets/corona_caveman_challenge.png')"
-        contain
-        height="50"
-        width="50"
-      />
 
-      <v-spacer></v-spacer>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Corona Caveman"
+          class="mr-2"
+          :src="require('./assets/corona_caveman_challenge.png')"
+          contain
+          width="50"
+          aspect-ratio="1"
+        />
+      </div>
+
+      <v-spacer />
+
+      <v-btn outlined large color="white">About us</v-btn>
+      <v-btn outlined large color="white">Cave wall of honor</v-btn>
+      <v-btn outlined large color="white">Donate</v-btn>
+
+      <v-spacer />
+
       <v-btn icon medium v-for="(item, index) of socialMedia" v-bind:key="index" :href="item.link">
         <v-icon>{{ item.icon }}</v-icon>
       </v-btn>
 
-      <v-btn href="mailto:coronacavemanchallenge@gmail.com" text>
+      <v-btn class="my-1" href="mailto:coronacavemanchallenge@gmail.com" text>
         <span class="mr-2">Contact Us</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <div
-      data-widget-src="https://secure.givelively.org/donate/blessings-in-a-backpack-inc/caveman-coronavirus-challenge?ref=sd_widget"
+      data-widget-src="https://secure.givelively.org/donate/blessings-in-a-backpack-inc/corona-caveman-challenge?ref=sd_widget"
       id="give-lively-widget"
       class="gl-branded-donation-widget"
     ></div>
@@ -40,16 +51,16 @@ export default {
       // Icon here refers to the name of Material Design Icons
       {
         icon: "mdi-twitter",
-        link: ""
-      },
-      {
-        icon: "mdi-facebook",
-        link: ""
-      },
-      {
-        icon: "mdi-instagram",
-        link: ""
+        link: "https://twitter.com/corona_caveman"
       }
+      // {
+      //   icon: "mdi-facebook",
+      //   link: ""
+      // },
+      // {
+      //   icon: "mdi-instagram",
+      //   link: ""
+      // }
       // {
       //   icon: "mdi-youtube",
       //   link: ""
@@ -59,7 +70,7 @@ export default {
   mounted() {
     let gl = document.createElement("script");
     gl.src =
-      "https://secure.givelively.org/widgets/branded_donation/blessings-in-a-backpack-inc/caveman-coronavirus-challenge.js";
+      "https://secure.givelively.org/widgets/branded_donation/blessings-in-a-backpack-inc/corona-caveman-challenge.js";
     document.getElementsByTagName("head")[0].appendChild(gl);
   }
 };
