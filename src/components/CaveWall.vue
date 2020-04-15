09e80d9637eb7a3ad57afe987c192378dff8aa18
Steven Kearns <skearns@vgimfs.com>
2020-04-15 18:51:21 -0400

    Create template for cavewall.

--- src/components/CaveWall.vue
+++ src/components/CaveWall.vue
@@ -1,5 +1,16 @@
 <template>
-  <v-app>wall of honor</v-app>
+  <v-app>
+    <v-container>
+      <v-row align="start" justify="center">
+        <h1 class="display-4 font-weight-bold my-5">CaveWall of Honor</h1>
+        <section class="headline">
+          Be sure to submit a picture with your caveman face on to any one of
+          our social media platforms, or to the email located in the “Contact
+          Us” button to be featured on the prestigious CaveWall of Honor!
+        </section>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
